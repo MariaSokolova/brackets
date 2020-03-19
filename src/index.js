87b9116de182782cca0c7c5c95ffd1ee0652a00d
= <=>
2020-03-20 00:22:03 +0100

    feat: implement JS brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let openBr = '';
+    let closeBr = '';
+    let pairs = {};
+    let equalBr = '';
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let el = bracketsConfig[i];
+        if (el[0] === el[1]) {
+            equalBr += el[0];
+            continue;
+        }
+        openBr += el[0];
+        closeBr += el[1];
+        pairs[el[1]] = el[0];
+    }
+
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+        const ch = str.charAt(i);
+        if (equalBr.includes(ch)) {
+            if (ch === stack[stack.length - 1]) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        if (openBr.includes(ch)) {
+            stack.push(ch);
+        }
+        if (closeBr.includes(ch)) {
+            if (pairs[ch] === stack[stack.length - 1]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
